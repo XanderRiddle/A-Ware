abe3cc41d504e5763b32d95efa68a35542af55f9
XanderRiddle <xriddle0917@gmail.com>
2025-03-30 19:47:59 -0400

    .

--- pwmexample.py
+++ pwmexample.py
@@ -7,23 +7,10 @@ CHIP_PATH = '/dev/gpiochip0'
 GPIO_OFFSET = 0  # CHANGE THIS to your actual GPIO offset
 CONSUMER = "pwm-test"
 
-# 1. Get the GPIO chip
-chip = gpiod.Chip(CHIP_PATH)
+chip = gpiod.Chip("/dev/gpiochip0")
+line = chip.request_lines(0)
+line.set_value(Value.ACTIVE)
 
-# 2. Create line request configuration
-config = gpiod.LineRequest()
-config.consumer = CONSUMER
-config.request_type = Direction.OUTPUT
-
-# 3. Get the line and make the request
-line = config.get_value()
-chip.request_lines(line)  # This creates the LineRequest object
-
-# 4. Now we can control the line
-print(f"Setting GPIO {GPIO_OFFSET} to HIGH")
-line.set_value(Value.ACTIVE)  # 3.3V output
-
-# Keep it running
 try:
     while True:
         pass
