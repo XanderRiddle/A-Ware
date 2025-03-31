d96d605fa099a936de5bbe8b4ba31bd1a3a9d2d3
XanderRiddle <xriddle0917@gmail.com>
2025-03-30 20:59:47 -0400

    a

--- pwmexample.py
+++ pwmexample.py
@@ -8,7 +8,7 @@ with gpiod.Chip("/dev/gpiochip0") as chip:
             output_value=gpiod.line.Value(0)
         )
     }
-    request = chip.request_lines(config=config)
-    request.set_value("gpio-5", gpiod.line.Value(0))  # Set high
+    request = chip.request_lines(config=config, consumer="test-pin22")
+    request.set_value(5, gpiod.line.Value(0))  # Set high
     input("Press Enter to exit...")  # Keeps the line held high
 # Line released automatically after 'with' block
