9c7a23747d01750c923f2c806f471fab2b2e7d4b
XanderRiddle <xriddle0917@gmail.com>
2025-03-30 21:52:06 -0400

    a

--- pwmexample.py
+++ pwmexample.py
@@ -2,7 +2,12 @@ import gpiod
 
 # Request pin 22 (GPIO5_C3, offset 179)
 with gpiod.Chip("/dev/gpiochip0") as chip:
-    config = {}
+    config = {
+        5: gpiod.LineSettings(
+            direction=gpiod.line.Direction.OUTPUT,
+            output_value=gpiod.line.Value(1)
+        )
+    }
     request = chip.request_lines(config=config, consumer="test-pin22")
     request.set_value(5, gpiod.line.Value(0))  # Set high
     input("Press Enter to exit...")  # Keeps the line held high
