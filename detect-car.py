98e83268c2597b4decbf0635a3b514191bf80ce6
Max Pethel <mpethel6@gatech.edu>
2025-04-16 01:41:17 -0400

    received

--- detect-car.py
+++ detect-car.py
@@ -182,8 +182,9 @@ def match_tracks(dets, tracks):
 
 # --- Callbacks ---
 def detection_cb(pkt: DetectionPacket):
+    print("RECEIVED DETECTIONS")
     global detections
-    detections = decode(pkt.nnData)
+    detections = decode(pkt.detections)
     update_tracks()
 
 def depth_cb(pkt: DetectionPacket):
